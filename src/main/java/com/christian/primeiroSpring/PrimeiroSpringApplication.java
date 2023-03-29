74ec69b88e392cd35ef4cd98ca0bdc808bb7f313
Christian Ricci <riccichristian338@gmail.com>
2023-03-29 18:50:11 -0300

    Primeiro Commit

---
+++ src/main/java/com/christian/primeiroSpring/PrimeiroSpringApplication.java
@@ -0,0 +1,13 @@
+package com.christian.primeiroSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/christian/primeiroSpring/PrimeiroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christian.primeiroSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
