4f2188dbcbd4a09d735f19dabd0cdcdf2d7da2da
Christian Ricci <riccichristian338@gmail.com>
2023-04-01 11:28:42 -0300

    Models Task and User added

---
+++ src/main/java/com/christian/primeiroSpring/models/Task.java
@@ -0,0 +1,112 @@
+package com.christian.primeiroSpring.models;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = Task.TABLE_NAME)
+public class Task {
+    public static final String TABLE_NAME = "task";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    @Column(name = "description", length = 255, nullable = false)
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 255)
+    private String description;
+
+
+    public Task() {
+    }
+
+    public Task(Integer id, User user, String description) {
+        this.id = id;
+        this.user = user;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Task id(Integer id) {
+        setId(id);
+        return this;
+    }
+
+    public Task user(User user) {
+        setUser(user);
+        return this;
+    }
+
+    public Task description(String description) {
+        setDescription(description);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+        return true;
+    if (obj == null)
+        return false;
+    if (!(obj instanceof Task))
+        return false;
+    Task other = (Task) obj;
+    if (this.id == null)
+        if (other.id != null)
+            return false;
+        else if (!this.id.equals(other.id))
+            return false;
+    return Objects.equals(this.id, other.id) && Objects.equals(this.user, other.user)
+            && Objects.equals(this.description, other.description);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+
+}
---
+++ src/main/java/com/christian/primeiroSpring/models/User.java
@@ -0,0 +1,118 @@
+package com.christian.primeiroSpring.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+    public interface createUser{}
+    public interface updateUser{}
+
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Integer id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = createUser.class)
+    @NotEmpty(groups = createUser.class)
+    @Size(groups = createUser.class, min = 2, max = 100)
+    private String username;
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name = "password", length = 60, nullable = false)
+    @NotNull(groups = {createUser.class, updateUser.class})
+    @NotEmpty(groups = {createUser.class, updateUser.class})
+    @Size(groups = {createUser.class, updateUser.class}, min = 8, max = 60)
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
+
+    public User() {
+    }
+
+    public User(Integer id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+        return true;
+    if (obj == null)
+        return false;
+    if (!(obj instanceof User))
+        return false;
+    User other = (User) obj;
+    if (this.id == null)
+        if (other.id != null)
+            return false;
+        else if (!this.id.equals(other.id))
+            return false;
+    return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username)
+            && Objects.equals(this.password, other.password);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+
+}
