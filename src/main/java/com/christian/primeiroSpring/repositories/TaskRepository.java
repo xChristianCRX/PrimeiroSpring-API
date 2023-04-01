4e3d3aeb83cc9d8c8bacbb2fd2f90a1211da6fda
Christian Ricci <riccichristian338@gmail.com>
2023-04-01 14:08:35 -0300

    Repositories User and Task added

---
+++ src/main/java/com/christian/primeiroSpring/repositories/TaskRepository.java
@@ -0,0 +1,22 @@
+package com.christian.primeiroSpring.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.christian.primeiroSpring.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer>{
+
+    List<Task>findByUser_Id(Integer id);
+
+    /* @Query(value = "SELECT t FROM Task t where t.user.id = :user_id")
+    List<Task>findByUserId(@Param("user_id")Integer user_id); */
+
+    /* @Query(value = "SELECT * FROM task t where t.user_id = :id", nativeQuery = true)
+    List<Task>findByUserId(@Param("id")Integer id); */
+}
