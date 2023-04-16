5b3b0f6b16a7caf37ea3363eed754d233edb7081
Christian Ricci <riccichristian338@gmail.com>
2023-04-16 15:03:17 -0300

    Lombok added

--- src/main/java/com/christian/primeiroSpring/models/Task.java
+++ src/main/java/com/christian/primeiroSpring/models/Task.java
@@ -1,7 +1,5 @@
 package com.christian.primeiroSpring.models;
 
-import java.util.Objects;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,9 +11,20 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Entity
 @Table(name = Task.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Task {
     public static final String TABLE_NAME = "task";
 
@@ -34,79 +43,5 @@ public class Task {
     @Size(min = 1, max = 255)
     private String description;
 
-
-    public Task() {
-    }
-
-    public Task(Integer id, User user, String description) {
-        this.id = id;
-        this.user = user;
-        this.description = description;
-    }
-
-    public Integer getId() {
-        return this.id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return this.user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Task id(Integer id) {
-        setId(id);
-        return this;
-    }
-
-    public Task user(User user) {
-        setUser(user);
-        return this;
-    }
-
-    public Task description(String description) {
-        setDescription(description);
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this)
-        return true;
-    if (obj == null)
-        return false;
-    if (!(obj instanceof Task))
-        return false;
-    Task other = (Task) obj;
-    if (this.id == null)
-        if (other.id != null)
-            return false;
-        else if (!this.id.equals(other.id))
-            return false;
-    return Objects.equals(this.id, other.id) && Objects.equals(this.user, other.user)
-            && Objects.equals(this.description, other.description);
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-        return result;
-    }
 
 }
--- src/main/java/com/christian/primeiroSpring/repositories/UserRepository.java
+++ src/main/java/com/christian/primeiroSpring/repositories/UserRepository.java
@@ -1,11 +1,9 @@
 package com.christian.primeiroSpring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.christian.primeiroSpring.models.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 
     //User findByUsername(String username);
